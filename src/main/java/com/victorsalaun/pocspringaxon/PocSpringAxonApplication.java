0d8e5df9112bafd55823df17ce3be41b800fd1cd
victor.salaun <victor.salaun@gmail.com>
2018-02-04 22:28:43 +0100

    init project with spring

---
+++ src/main/java/com/victorsalaun/pocspringaxon/PocSpringAxonApplication.java
@@ -0,0 +1,12 @@
+package com.victorsalaun.pocspringaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringAxonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringAxonApplication.class, args);
+	}
+}
